985d0a446210266d9bd1dd40fa165eb6bdcb762b
Costa Anton <costaanton96@gmail.com>
2019-01-12 18:46:40 +0200

    public

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Transform cameraTransform;
+    public Transform cameraTransform;
     [SerializeField] Weapon weapon;
     Quaternion targetRotation;
 
