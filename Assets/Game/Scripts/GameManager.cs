aa8b382911fc17f1e62ae1de6561399ac2b92eef
Florian-Claudiu Gheorghica <claudiu_fg@yahoo.com>
2019-01-18 18:00:53 +0200

    GameManager Commit
    Added GameManager, Loader, Stop Behavior edits
    Removed Player from Scene, adding it programatically.
    Added a player spawn point in scene, for player spawning.

---
+++ Assets/Game/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = null;
+    private GameObject player;
+    [SerializeField] public GameObject playerSpawnPoint;
+    [SerializeField] GameObject playerPrefab;
+
+    #region Properties
+    public GameObject Player { get => player; }
+    #endregion
+
+    private void Awake()
+    {
+        Debug.Log("GameManager is Awake");
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        SetupScene();
+    }
+
+    private void SpawnPlayer()
+    {
+        if(playerSpawnPoint == null)
+        {
+            playerSpawnPoint = Loader.Instance.PlayerSpawnPoint;
+        }
+        playerPrefab = Instantiate(playerPrefab);
+        playerPrefab.transform.position = playerSpawnPoint.transform.position;
+        playerPrefab.transform.rotation = playerSpawnPoint.transform.rotation;
+    }
+
+    private void SpawnEnemies()
+    {
+
+    }
+
+    private void SetupScene()
+    {
+        SpawnPlayer();
+        SpawnEnemies();
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Game/Scripts/Loader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Class used to load scene elements correctly within the level.
+/// </summary>
+public class Loader : MonoBehaviour
+{
+    public GameObject gameManager;
+    public GameObject playerSpawnPoint;
+    public static Loader Instance = null;
+
+    public GameObject PlayerSpawnPoint
+    {
+        get
+        {
+            return playerSpawnPoint;
+        }
+    }
+
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        if(GameManager.Instance == null)
+        {
+            Instantiate(gameManager);
+            GameManager.Instance.playerSpawnPoint = playerSpawnPoint;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
